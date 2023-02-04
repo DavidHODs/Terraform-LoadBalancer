6ee3e6be334b8fd62f52ec6d991c8571b1da905f
DavidHODs <davidoluwatobi41@gmail.com>
2023-02-04 16:40:33 +0100

    minor tweak to add ipv4 dns so ansible can connect with the instances

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "terra-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true 
+  enable_dns_hostnames = true 
+  assign_generated_ipv6_cidr_block = true 
   instance_tenancy = "default"
 
   tags = {
